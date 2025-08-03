0e2bbd6cd5f79e3571e4a69829d53b460a55f3d8
Felix <feudeine@sevnbytes.com>
2025-08-03 19:09:23 +0100

    provider

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -20,6 +22,7 @@ const menuOptions = [
 ];
 
 function Header() {
+  const { user } = useUser();
   return (
     <div className="flex justify-between items-center p-4">
       {/* Logo*/}
@@ -38,9 +41,15 @@ function Header() {
         ))}
       </div>
       {/* Get Started Button */}
-      <SignInButton mode="modal">
-        <Button>Get Started</Button>
-      </SignInButton>
+      {!user ? (
+        <SignInButton mode="modal">
+          <Button>Get Started</Button>
+        </SignInButton>
+      ) : (
+        <Link href="/create-trip" passHref>
+          <Button>Create New Trip</Button>
+        </Link>
+      )}
     </div>
   );
 }
--- app/provider.tsx
+++ app/provider.tsx
@@ -1,17 +1,57 @@
-import React from "react";
-import Header from "./_components/Header";
+"use client";
 
-function Provider({
-  children,
-}: Readonly<{
+import React, { useContext, useEffect, useState } from "react";
+import Header from "./_components/Header";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useUser } from "@clerk/nextjs";
+import { UserDetailContext } from "@/context/UserDetailContext";
+
+type ProviderProps = {
   children: React.ReactNode;
-}>) {
+};
+
+const Provider: React.FC<ProviderProps> = ({ children }) => {
+  const createUser = useMutation(api.user.CreateNewUser);
+  const { user } = useUser();
+  const [userDetail, setUserDetail] = useState<any>();
+
+  // Define the function before using it in useEffect
+  const createNewUser = async () => {
+    if (user) {
+      try {
+        const result = await createUser({
+          email: user?.primaryEmailAddress?.emailAddress ?? "",
+          imageUrl: user?.imageUrl,
+          name: user?.fullName ?? "",
+        });
+
+        setUserDetail(result);
+      } catch (error) {
+        console.error("Failed to create user:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      createNewUser();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
   return (
-    <div>
-      <Header />
-      {children}
-    </div>
+    <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+      <div>
+        <Header />
+        {children}
+      </div>
+    </UserDetailContext.Provider>
   );
-}
+};
 
 export default Provider;
+
+export const useUserDetail = () => {
+  return useContext(UserDetailContext);
+};
